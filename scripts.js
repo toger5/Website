6d8f9d874b875cc788215a65ce6b3cd057f2d48e
toger5 <toger5@hotmail.de>
2016-04-18 14:03:25 +0200

    improved indents

--- scripts.js
+++ scripts.js
@@ -2,11 +2,9 @@
 
 /*was muss nun noc gemacht werden
 also
-aus inspect id muss ne liste werde
-dann muss beim resett jeder teil der liste wieder zurück gedreht werden
-wenn man rotate to next aufruft dann soll zu dem inspect string das element dazugamcht werden 
+wenn man rotate to next aufruft dann soll zu dem inspect string das element dazugamcht werden
 über all wo ich dann den inspect strin aufrfe will ich dann immmer nur das letzte elemnt des string verwenden
-das hat den vorteil dass ich jes div dann immmer nur einmal drehen kann und dass ich alle divs wieder zurück drehen kann.
+das hat den vorteil dass ich jedes div dann immmer nur einmal drehen kann und dass ich alle divs wieder zurück drehen kann.
 
 dann muss nich eine Funktin gschrieben werden die es ermöglicht dass alle elemente mit dem sleben namen (die zu einem gemiensamen artikel gehören)
 gefunden werde und an die selbe stele bewegt werden.
@@ -22,20 +20,19 @@ window.onscroll = testScroll
 $(document).ready(function(){
 	$( "body" ).click(function( event ) {
 		var tar = $(event.target)
-		click(tar);
+		page_click(tar);
 	});
-}); 
+});
 /*für jeden Artikel muss man eine zeile an code hinzufügen, damit die ganzen artik an die rihtige stelle verschoben werden. für jeden Artikel braucht es eine andere ID*/
 
 function getInspected(){
 	return inspectID[page]
 }
+
 function getInspectedNameOnly(){
 	n = inspectID[0]
 	return n.slice(0,n.length - 2)
 }
-
-	
 
 function inspect(){
 	if (inspectID.length == 0){
@@ -43,7 +40,8 @@ function inspect(){
 	}
 	return true;
 }
-function click(target){
+
+function page_click(target){
 	targetID = target.attr("id");
 	if(targetID === undefined){
 		console.log("nnnnnnnnnnnnnnnn");
@@ -60,10 +58,10 @@ function click(target){
 		}
 	}
 }
+
 function rotateToNextPage(){
-	
 	if(page < inspectID.length - 1){
-		console.log("es wurde page: " + page + " gedraht")
+		console.log("es wurde page: " + page + " gedreht")
 		amount = 90 - 2.5 * (page + 1);
 		$("#" + getInspected()).css({ transformOrigin: '0px 0px' }).transition({
 			rotate : "-=" + amount + "deg"
@@ -72,7 +70,7 @@ function rotateToNextPage(){
 	}
 }
 function rotateToLastPage(){
-	
+
 	$("#"+inspectID[page - 1]).css({ transformOrigin: '0px 0px' }).transition({
 		rotate : "0deg"
 	}, 800, "easeOutExpo");
@@ -80,7 +78,7 @@ function rotateToLastPage(){
 	//console.log("laenge der liste: "  + inspectID.length + " somit: " + getInspected());
 	//inspectID.pop();
 	//console.log("laenge der liste: "  + inspectID.length + " somit: " + getInspected());
-	 
+
 }
 
 /*
@@ -93,10 +91,10 @@ function callToForeground(){
 	/*alert(window.innerHeight);
 	var strHeight = toString(window.innerHeight) + "px";
 	alert(strHeight);
-	
+
 	newMainWindow.style.height = window.innerHeight.toString() + "px";
 	newMainWindow.style.zIndex = lastobj.style.zIndex - 1;
-	
+
 	newMainWindow.style.top = coverobj.offsetTop + "px";
 	console.log(lastobj.style.zIndex - 1);
 }
@@ -114,7 +112,7 @@ function gotoPage(numberToGo){
 	}
 }
 function testScroll(ev){
-	
+
 	if (inspect()){
 		resetInspect(false);
 	}
@@ -127,7 +125,7 @@ function resetInspect(scroll){
 			/*d = (inspectID.length - index - 1) * 150*/
 			//document.getElementById(inspectID[index]).style.zIndex -= 100;
 			if(index == 0){
-				
+
 				$("#"+inspectID[index]).css({ transformOrigin: '0px 0px' }).transition({
 					rotate : "0deg"/*, delay: d*/
 				}, 800, "easeOutExpo");
@@ -135,16 +133,16 @@ function resetInspect(scroll){
 					height : "600px"/*, delay: d*/
 				}, 800);
 				if(scroll){
-					console.log("es scrollt !!!!!!!!!!!!!!!!!!!!!!!!!")
+					console.log("es scrollt")
 					var e = document.getElementById(inspectID[0]);
-					
+
 					posTo = e.offsetTop - (window.innerHeight - 600)/2;
 					setTimeout(function(){
 						$("body,html").animate({
 							scrollTop : posTo
 						}, 800);
 					}, 800);
-					
+
 				/*}else{
 					$("#"+inspectID[index]).css({ transformOrigin: '0px 0px' }).transition({
 						height : "600px"
@@ -152,15 +150,15 @@ function resetInspect(scroll){
 				}
 			}
 			else{
-				
+
 				$("#"+inspectID[index]).css({ transformOrigin: '0px 0px' }).transition({
 					rotate : "0deg"/*, delay: d*/
-					}, 800, "easeOutExpo");		
+					}, 800, "easeOutExpo");
 			}
 	}
 	/*if(scroll){
 		var e = document.getElementById(inspectID[0]);
-		
+
 		posTo = e.offsetTop - 200 /*((parseInt(window.innerHeight) - parseInt(e.style.height))/2)*/;
 		/*$("body,html").animate({
 			scrollTop : posTo, delay : 800
@@ -172,7 +170,7 @@ function scrollToItem(itemName){
 	if(!inspect()){
 		var e = document.getElementById(itemName);
 		scrollAnimationTo(e.offsetTop);
-		sizeToWindow(itemName);	
+		sizeToWindow(itemName);
 		setTimeout(function(){
 			fillInspectID(itemName);
 		}, 600);
@@ -186,23 +184,23 @@ function fillInspectID(Name0){
 	for	(index = 0; index < leist.length; index++){
 		leist[index].style.zIndex = 0
 		//alert(leist[index].id + " " + leist[index].style.zIndex)
-		
+
 	}
 	var filling = true;
 	while(filling){
 		//neue ID Nummer Bauen
-		
-		
+
+
 		var nummerStr = nummer;
 		if (nummerStr.toString().length == 1){nummerStr = "0" + nummerStr;}
-		
+
 		//neuen IdNamen bauen
 		var NeuID = name + nummerStr;
-		
+
 		//neuen Namen checken
 		if(document.getElementById(NeuID) != null){
 			inspectID.push(NeuID);
-			
+
 			newMainWindow = document.getElementById(NeuID);
 			coverobj = document.getElementById(inspectID[0]);
 			newMainWindow.style.zIndex = 101 - nummer;
@@ -214,9 +212,9 @@ function fillInspectID(Name0){
 		}
 		else{filling = false}
 		nummer += 1;
-		
+
 	}
-	
+
 	console.log("das ist die erstellte isnepct liste für dieses kapitel:  " + inspectID)
 }
 function scrollAnimationTo(positionTo){
@@ -228,7 +226,7 @@ function scrollAnimationTo(positionTo){
 }
 
 function sizeToWindow(iName){
-	
+
 	$("#"+iName).animate({
 		height : window.innerHeight
 	},500,"easeOutExpo")
@@ -238,7 +236,7 @@ function sizeToWindow(iName){
 function gototop(){
 	resetInspect(false);
 	scrollAnimationTo(0);
-	
+
 }
 
 
